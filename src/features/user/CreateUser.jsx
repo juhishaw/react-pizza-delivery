e5426d07924a7392eb367c2b1cdd43a6e9eadf81
juhi shaw <jshaw@ciena.com>
2023-10-10 11:49:15 +0530

    redux, thunk, fetch, tailwind

---
+++ src/features/user/CreateUser.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import Button from '../../ui/Button';
+import { useDispatch } from 'react-redux';
+import { updateName } from './userSlice';
+import { useNavigate } from 'react-router-dom';
+
+function CreateUser() {
+  const [username, setUsername] = useState('');
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    //Update redux store
+    if(!username) return;
+
+    dispatch(updateName(username))
+    navigate('/menu')
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+    <p className="mb-4 text-sm text-stone-600 md:text-base">
+      👋 Welcome! Please start by telling us your name:
+    </p>
+
+    <input
+      type="text"
+      placeholder="Your full name"
+      value={username}
+      onChange={(e) => setUsername(e.target.value)}
+      className="w-72 m-6 rounded-full px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 sm:w-64 sm:focus:w-72"
+    />
+
+    {username !== '' && (
+      <div>
+        <Button type="primary">Start ordering</Button>
+      </div>
+    )}
+  </form>
+  );
+}
+
+export default CreateUser;
